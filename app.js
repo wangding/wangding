b0be59798ef597a4c4e359bf59e70e9f32a368f8
王顶 <408542507@qq.com>
2017-01-23 13:38:32 +0800

    初始代码

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('王顶是一位开源教育工作者。');
+console.log('王顶的 QQ 号：408542507');
+console.log('王顶的 email：408542507@qq.com');
+console.log('王顶的 Github 个人主页地址：https://github.com/wangding/');
+console.log('王顶的 51CTO 学院课程中心：http://edu.51cto.com/lecturer/index/user_id-8606427.html');
+
